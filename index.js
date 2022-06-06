c296983e267dbec8e3f18db0d2de1fb92bac101a
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-06-06 22:28:39 +0300

    Completed the Lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1=31;
+const num2= 2;
+const multiply = num1*num2;
+const random = Math.ceil(Math.random()) + 1;
+
+const num3 = 9;
+const num4= 5;
+const mod = num3%num4;
+
+const max= Math.max(2,3,7,16,19,20,14);
+
